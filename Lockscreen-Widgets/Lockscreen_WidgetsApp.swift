a403bd689bd1e61f9efd382737ee4a020da32c96
Bogdan Barbus <47646017+bogrim19@users.noreply.github.com>
2023-05-12 12:42:48 +0300

    Initial Commit

---
+++ Lockscreen-Widgets/Lockscreen_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lockscreen_WidgetsApp.swift
+//  Lockscreen-Widgets
+//
+//  Created by Bogrim on 12.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lockscreen_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
